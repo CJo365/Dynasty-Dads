161a5bdfe36033143685b43c42249a23f9f7ab93
CJo365 <116183671+CJo365@users.noreply.github.com>
2023-06-19 15:31:50 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "91683839878356736"; // your league ID
+export const leagueID = "916838398378356736"; // your league ID
 export const leagueName = "Dynasty Dads 4"; // your league name
 export const dues = 25; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
