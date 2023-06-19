1271da6c4124e25ddaaf705d975b273aac761cfa
CJo365 <116183671+CJo365@users.noreply.github.com>
2023-06-19 15:39:18 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,13 +1,52 @@
 /*   STEP 1   */
 export const leagueID = "916838398378356736"; // your league ID
 export const leagueName = "Dynasty Dads 4"; // your league name
-export const dues = 25; // (optional) used in template constitution page
+export const dues = 20; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 /*   STEP 2   */
 export const homepageText = `
-  <p>Checkout the Dynasty Dads Podcast here: https://open.spotify.com/show/3g67KnEr2JaIzbjuhuIJMn?si=dce6a8049ca34093</p>
+  <p>Introduction
+
+
+Dynasty Dads leagues operate very differently than a vast majority of communities. We believe in the individuality of each separate league. While each league will operate under a similar umbrella, each league will have its own set of rules and settings voted on pre-draft by all leaguemates. This gives power to the individual leagues and owners to make each league unique. If there is something you want to bring up for a vote feel free, at the conclusion of every season pretty much everything is fair game. Depending on the gravity of the rule change a vote could pass with as simple of a majority vote, others may require a unanimous vote, this is up to discretion of the league commish. While each league will be unique, there will be a base set of rules at every level of a league. With this being said NO RULES WILL CHANGE MID-SEASON FOR ANY REASON
+
+
+League Buy-in and payout structure
+
+All buy-ins and payouts will go through Leaguesafe, the buy-in amount and payout scale will be voted on by the league
+Every Leaguesafe will be paid out by a majority vote, not commish controlled
+Every league will stay paid up a year in advance
+All league dues for the following year will need to be paid before rookie draft
+
+
+Roster Construction
+
+Every league will start out as a PPR Superflex Tight end premium league with 10 starting slots (1QB, 2RBs, 3WRs, 1TE, 2 Flex and 1 SF) from there if any changes want to be made they can be voted on by the league
+All leagues will start with 15 bench slots, 5 IR, 15 Taxi, from there if any changes want to be made they can be voted on by the league
+Everyone must start a valid lineup every week, don't be that guy (no IR or out guys starting) if you have to be reminded 3 times in a season you will be replaced
+
+
+Startup draft
+
+Every draft will default to a 25 round snake draft with a 3rd Round Reversal, from there if any changes want to be made they can be voted on by the league
+Every draft will be a slow draft with a 4, 8 or 12 hour timer voted on by the league, the timer will pause overnight at 11pm cst and resume at 7 am cst
+
+
+Rookie Draft and Picks
+
+All rookie drafts will be a 4 round slow draft with a 4, 8 or 12 hour timer voted on by the league, the timer will pause overnight at 11pm cst and resume at 7 am cst
+If you trade away a future rookie pick you are responsible for that years dues at the time of the trade
+Draft order for teams that miss the playoffs will be determined by reverse order of max possible points, for the teams that make the playoffs the draft order will be determined by reverse playoff finish, the winner of the 3rd and 5th place matchups will receive the better draft pick
+
+
+Waivers, Trades and Refunds
+
+Waivers- every league will start as a FAAB league with 100 or 1000 budget voted on by the league, there will be and offseason and in season budget. Offseason budget will reset at the start of every season and the in season budget will reset when the league rolls over into the new year. All offseason waivers will run daily, in season will run every tuesday after all games have concluded, after waivers run it will be open FA until that a players game starts
+Trades- trades will lock before the start of the playoffs unless voted otherwise by the league, no trades will be vetoed unless the commish sees obvious collusion, any collusion will result in removal from the league with no refund, All trades will go to pending after accepted, both parties must say "approve/accept" in the chat then it will be pushed through
+Refunds- if you want to leave after the season as long as you have not traded away future rookie picks you will receive a refund for the dues you have paid. Leaving midseason you are not entitled to your money back from that current season
+</p>
  `;
 
 /*   STEP 3   */
